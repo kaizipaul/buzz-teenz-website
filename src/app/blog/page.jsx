e1da45849467dc6b9448fb335b141297fb709afc
Paul Kaizirege <kaizipaul@gmail.com>
2024-05-27 11:23:54 +0300

    add ui to blog page and blog details

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,4 +1,17 @@
-export default function Blog () {
+"use client"
+import { useEffect, useState } from 'react';
+import AllPosts from '@/components/blogcards/allposts';
+import MainStory from '@/components/blogcards/mainstory';
+import fetchBlogPosts from '../helpers/fetchBlogs';
+
+const Blog = () => {
+ const [blogs, setBlogs] = useState([]);
+
+ useEffect (() => {
+  fetchBlogPosts()
+  .then(setBlogs)
+ }, [])
+
   return (
     <>
    <section>
@@ -15,37 +28,66 @@ export default function Blog () {
     </div>
    </section>
    <section>
-   <div class="grid grid-rows-2 grid-flow-col gap-2 h-[400px]">
-          <div className="row-span-2 border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div> 
+   <div class="grid grid-rows-2 grid-flow-col gap-2 h-[400px] text-left">
+    {/* {featuredBlogs.data.map (featuredBlog => {
+      <MainStory 
+      key={featuredBlog.id}
+      title={featuredBlog.attributes.title}
+      author={featuredBlog.attributes.author}
+      />
+    })} */}
+          <div className="row-span-2">
+            <MainStory
+            title={'This is Teen Buzz'}
+            author={'Marcus Johnson'}
+            date={'24 Dec 2024'}
+            tag={'The Finals'}
+            />
+          </div>
+          <div>
+          <MainStory
+            title={'This is Teen Buzz'}
+            author={'Marcus Johnson'}
+            date={'24 Dec 2024'}
+            tag={'The Finals'}
+            />
+          </div>
+          <div>
+          <MainStory
+            title={'This is Teen Buzz'}
+            author={'Marcus Johnson'}
+            date={'24 Dec 2024'}
+            tag={'The Finals'}
+            />
+            </div> 
         </div>
    </section>
    <section>
-    <div>
+    <div className='flex flex-col gap-8'>
       <h2>
         All Posts.
       </h2>
-      <div class="grid grid-rows-4 grid-flow-col gap-2 h-[400px]">
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
-          <div className="border-solid border-2"></div>
+      <div className="grid grid-cols-4 grid-flow-row gap-2 h-[800px] text-left">
+      {blogs.map(blog => (
+       <AllPosts
+       key={blog.id}
+       thumbnail={`http://localhost:1337${blog.attributes.thumbnail.data.attributes.url}`}
+       title={blog.attributes.title}
+       author={'Marcus Johnson'}
+       date={'24 Dec 2024'}
+       tag={blog.attributes.tags}
+       link={`blog/${blog.attributes.slug}`}
+       />
+      ))}
         </div>
     </div>
    </section>
    </>
   )
 }
+
+export default Blog;
+
+// TO DO:
+// 1. render all blogposts here
+//
