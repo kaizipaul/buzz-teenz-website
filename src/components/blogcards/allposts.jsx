4cb69022ee92483b7356fcd425bd6aff97ce55f5
Paul Kaizirege <kaizipaul@gmail.com>
2024-05-29 08:28:45 +0300

    modify news and events components

--- src/components/blogcards/allposts.jsx
+++ src/components/blogcards/allposts.jsx
@@ -36,9 +36,10 @@ const AllPosts = ({ thumbnail, title, author, date, tag, link }) => {
 const styles = {
   card: {
     width: '100%',
-    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
     borderRadius: '10px',
     overflow: 'hidden',
+    boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
   },
   coverPhotoContainer: {
     width: '100%',
--- src/components/eventcards/maincard.jsx
+++ src/components/eventcards/maincard.jsx
@@ -1,9 +1,87 @@
 // components/Card.js
 import Image from 'next/image';
+import Link from 'next/link';
 
-const MainCard = ({ title, location, date, coverPhoto, tag }) => {
+const MainCard = ({ title, location, date, thumbnail, tag, link }) => {
+  const backgroundImage = { 
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.9)), url(${thumbnail})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+   }
+
+   const styles = {
+    card: {
+      width: '100%',
+      height: '100%',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      borderRadius: '10px',
+      overflow: 'hidden',
+      position: 'relative',
+      boxShadow: 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
+    },
+    overlay: {
+      // backgroundColor: 'rgba(255, 255, 255, 0.3)',
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-end',
+      padding: '20px'
+    },
+    tag: {
+      backgroundColor: '#000',
+      color: '#fff',
+      padding: '5px 10px',
+      borderRadius: '15px',
+      display: 'inline-block',
+      fontSize: '12px',
+      marginBottom: '10px',
+      width: '80px',
+    },
+    readTime: {
+      fontSize: '12px',
+      color: '#999',
+      marginBottom: '10px'
+    },
+    title: {
+      fontSize: '18px',
+      fontWeight: 'bold',
+      marginBottom: '15px'
+    },
+    authorSection: {
+      display: 'flex',
+      alignItems: 'center'
+    },
+    avatar: {
+      width: '40px',
+      height: '40px',
+      borderRadius: '50%',
+      marginRight: '10px'
+    },
+    authorInfo: {
+      display: 'flex',
+      flexDirection: 'column'
+    },
+    authorName: {
+      fontSize: '14px',
+      fontWeight: 'bold'
+    },
+    date: {
+      fontSize: '12px',
+      color: '#999'
+    }
+   };
+
+   const combined = {
+    ...styles.card,
+    ...backgroundImage,
+   }
+
+
   return (
-    <div style={styles.card}>
+    <Link href={`${link}`}>
+    <div style={combined}>
       <div style={styles.overlay}>
         <div style={styles.tag}>{tag}</div>
         <div style={styles.title}>{title}</div>
@@ -16,73 +94,11 @@ const MainCard = ({ title, location, date, coverPhoto, tag }) => {
         </div>
       </div>
     </div>
+    </Link>
   );
 };
 
-const styles = {
- card: {
-   width: '100%',
-   height: '100%',
-   backgroundSize: 'cover',
-   backgroundPosition: 'center',
-   borderRadius: '10px',
-   overflow: 'hidden',
-   position: 'relative',
-   backgroundImage: 'url("../assets/hero.jpg")',
-   // backgroundImage: `url(${coverPhoto})`
- },
- overlay: {
-   backgroundColor: 'rgba(255, 255, 255, 0.3)',
-   width: '100%',
-   height: '100%',
-   display: 'flex',
-   flexDirection: 'column',
-   justifyContent: 'flex-end',
-   padding: '20px'
- },
- tag: {
-   backgroundColor: '#000',
-   color: '#fff',
-   padding: '5px 10px',
-   borderRadius: '15px',
-   display: 'inline-block',
-   fontSize: '12px',
-   marginBottom: '10px',
-   width: '80px',
- },
- readTime: {
-   fontSize: '12px',
-   color: '#999',
-   marginBottom: '10px'
- },
- title: {
-   fontSize: '18px',
-   fontWeight: 'bold',
-   marginBottom: '15px'
- },
- authorSection: {
-   display: 'flex',
-   alignItems: 'center'
- },
- avatar: {
-   width: '40px',
-   height: '40px',
-   borderRadius: '50%',
-   marginRight: '10px'
- },
- authorInfo: {
-   display: 'flex',
-   flexDirection: 'column'
- },
- authorName: {
-   fontSize: '14px',
-   fontWeight: 'bold'
- },
- date: {
-   fontSize: '12px',
-   color: '#999'
- }
-};
+
 
 
 
