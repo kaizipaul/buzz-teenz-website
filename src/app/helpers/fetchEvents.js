f59130c7da25cdad84cc971429b96b81894969ca
Paul Kaizirege <kaizipaul@gmail.com>
2024-05-29 08:27:51 +0300

    modify api request params

--- src/app/helpers/fetchEvents.js
+++ src/app/helpers/fetchEvents.js
@@ -1,5 +1,5 @@
 export default async function fetchEvents(params) {
- const request = await fetch(`http://localhost:1337/api/events?populate[1]=coverimage&${params}`);
+ const request = await fetch(`http://localhost:1337/api/events?populate[1]=coverimage&populate[0]=authors&${params}`);
  const response = await request.json();
  // if (!response.ok) {
  //   throw new Error('Failed to fetch blog posts');
