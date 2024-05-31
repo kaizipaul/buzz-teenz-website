067b5fbb0a1904528ac3f44799b6aad3784e035e
Paul Kaizirege <kaizipaul@gmail.com>
2024-05-31 20:13:19 +0300

    optimize for mobile

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { FaBookOpen } from "react-icons/fa6";
 import { TbFileMusic } from "react-icons/tb";
 import { IoBulb } from "react-icons/io5";
 import MainCard from "@/components/eventcards/maincard";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -14,10 +15,13 @@ export default function Home() {
           CREATING EXCITING <br />
           EXPERIENCES
         </h1>
-        <p className="w-[500px]">
+        <p className="w-[90%] sm:w-[50%]">
         Lorem ipsum dolor sit amet stet. Ea consectetuer ipsum nonumy rebum autem consequat sit erat gubergren facilisis sit nibh magna.
         </p>
-        <Button variant="outline" className="dark">Vote Now
+        <Button className="bg-pink-600 mt-10">
+          <Link href='/blog'>
+            Read Our Blog
+          </Link>
         <BiLinkExternal className="ml-2" />
         </Button>
       </div>
@@ -28,22 +32,22 @@ export default function Home() {
         <h2>
           About Us.
         </h2>
-        <p className="w-[500px]">
+        <p className="w-[90%] sm:w-[50%]">
         Lorem ipsum dolor sit amet aliquyam takimata vel. Et ipsum ad. Erat sit erat et ipsum amet in rebum. Elitr vero sanctus magna id rebum dolores lorem option in consetetur sit duo amet diam augue.
         </p>
       </div>
-      <div className="flex gap-4 h-[300px] justify-center">
-        <div className="about-cards rounded-lg flex flex-col justify-evenly gap-8 w-[30%] px-[20px] py-[30px] about-cards">
+      <div className="flex flex-col h-[90%] items-center w-[100%] sm:flex-row gap-4 h-[90%] h-[300px] justify-center">
+        <div className="about-cards rounded-lg flex flex-col justify-evenly gap-8 px-[20px] py-[30px] w-[100%] sm:w-[30%]">
           <FaBookOpen className="text-[50px]" />
           <p className="text-left font-bold">Educate</p>
           <p className="text-left w-[90%]">Eum euismod eu dolore diam kasd ea et duo clita veniam dolores</p>
         </div>
-        <div className="about-cards rounded-lg flex flex-col justify-evenly gap-8 w-[30%] px-[20px] py-[30px] about-cards">
+        <div className="about-cards rounded-lg flex flex-col justify-evenly gap-8 px-[20px] py-[30px] w-[100%] sm:w-[30%]">
           <TbFileMusic className="text-[50px]" />
           <p className="text-left font-bold">Entertain</p>
           <p className="text-left w-[90%]">Eum euismod eu dolore diam kasd ea et duo clita veniam dolores</p>
         </div>
-        <div className="about-cards rounded-lg flex flex-col justify-evenly gap-8 w-[30%] px-[20px] py-[30px] about-cards">
+        <div className="about-cards rounded-lg flex flex-col justify-evenly gap-8 px-[20px] py-[30px] w-[100%] sm:w-[30%]">
           <IoBulb className="text-[50px]" />
           <p className="text-left font-bold">Inspire</p>
           <p className="text-left w-[90%]">Eum euismod eu dolore diam kasd ea et duo clita veniam dolores</p>
@@ -57,7 +61,7 @@ export default function Home() {
         See what we’ve been cookin up,<br />
         and what’s to come.
         </h2>
-        <div class="grid grid-rows-2 grid-flow-col gap-2 h-[400px] text-left">
+        <div class="grid grid-rows-4 h-[90%] sm:grid-rows-2 grid-flow-col gap-2 h-[400px] text-left">
           <div className="row-span-2">
           <MainCard
             tag={'The Finals'}
