10273296c158c83ddc035b7bf8cf02a5649a32bc
Paul Kaizirege <kaizipaul@gmail.com>
2024-06-01 15:22:17 +0300

    add font file

---
+++ src/app/fonts.js
@@ -0,0 +1,12 @@
+import { Barlow_Condensed, Inter, Roboto_Mono } from 'next/font/google'
+ 
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const barlow_condensed = Barlow_Condensed({
+  weight: ['800'],
+  subsets: ['latin'],
+  display: 'swap',
+})
